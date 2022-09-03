5c85adb8f4d4f0ecd854a4bd05ce309851c98f94
Gustavo Sansotta <sansotta.gustavo@gmail.com>
2022-09-03 13:43:49 -0300

    separo el contador en componente ItemCount

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,15 +1,6 @@
-import { useState } from 'react';
+import ItemCount from "./ItemCount"
 
 const Players = ({ nombre, imagen, posicion, precio }) => {
-  const [counter, setCounter] = useState(1)
-
-  const handleClick = () => {
-    setCounter(counter + 1)
-  }
-
-  const handleClickRest = () => {
-    setCounter(counter - 1)
-  }
 
   return (
     <div class="card w-96 bg-base-100 shadow-xl ">
@@ -21,16 +12,8 @@ const Players = ({ nombre, imagen, posicion, precio }) => {
         <h3>Posición: {posicion}</h3>
         <h3>Precio: ${precio}</h3>
         <strong>Años de contrato:</strong>
-        <div class="card-actions">
-          <div class="inline-flex">          
-            <button class="bg-gray-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={handleClickRest} disabled={counter === 0} >
-              -
-            </button>
-            <strong><h1 id='counter'>{counter}</h1></strong>
-            <button class="bg-gray-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={handleClick} disabled={counter === 4} >
-              +
-            </button>           
-          </div>
+        <div class="card-actions">  
+          <ItemCount/>
         </div>
         <button class="btn btn-primary">CONTRATAR</button>
       </div>
